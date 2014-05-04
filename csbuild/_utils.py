8f4baeebb1050dfbf28848dcde9e3c433687961d
brandon <brandon.m.bare@gmail.com>
2014-05-04 15:46:03 -0500

    - Removed some extraneous whitespace from setup.py. - Fixed not being able to install to Python3. The cause was Python3 having different syntax for octal numbers. - Fixed the exception that gets thrown when building on Windows. Running the vcvarsall batch files prints output that is not always in the form of key/value pairs, but our code was assuming each line would be in that form.

--- csbuild/_utils.py
+++ csbuild/_utils.py
@@ -209,7 +209,7 @@ class threaded_build( threading.Thread ):
 							data.write("\n")
 							lastLine += 1
 					if platform.system() == "Windows":
-						fd = os.open(self.file, os.O_WRONLY | os.O_CREAT | os.O_NOINHERIT, 0666)
+						fd = os.open(self.file, os.O_WRONLY | os.O_CREAT | os.O_NOINHERIT, oct(438))
 						os.write(fd, data.getvalue())
 						os.fsync(fd)
 						os.close(fd)
