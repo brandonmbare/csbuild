85aada02803af8d73ff906a72e9f8b622dc5d55e
Brandon Bare <brandon.m.bare@gmail.com>
2014-12-14 21:56:39 -0600

    -- Added a new package called vsgen to handle plugins for the Visual Studio project genetor. -- Fixed up some of the spacing in the new vs generator and imported the new platform plugins.

--- csbuild/project_generator_visual_studio_new.py
+++ csbuild/project_generator_visual_studio_new.py
@@ -28,6 +28,9 @@ from . import projectSettings
 from . import log
 from . import _shared_globals
 
+from .vsgen.platform_windows import *
+from .vsgen.platform_android import *
+
 
 class VisualStudioVersion:
 	v2010 = 2010
@@ -61,7 +64,7 @@ def GenerateNewUuid( uuidList, name ):
 
 		# Name collision!  The easy solution here is to slightly modify the name in a predictable way.
 		nameToHash = "{}{}".format( name, nameIndex )
-		++nameIndex
+		nameIndex += 1
 
 
 def CorrectConfigName( configName ):
@@ -147,13 +150,15 @@ class project_generator_visual_studio( project_generator.project_generator ):
 
 	@staticmethod
 	def AdditionalArgs( parser ):
-		parser.add_argument( "--vs-gen-version",
+		parser.add_argument(
+			"--vs-gen-version",
 			help = "Select the version of Visual Studio the generated solution will be compatible with.",
 			choices = VisualStudioVersion.All,
 			default = GetImpliedVisualStudioVersion(),
 			type = int,
 		)
-		parser.add_argument( "--vs-gen-replace-user-files",
+		parser.add_argument(
+			"--vs-gen-replace-user-files",
 			help = "When generating project files, do not ignore existing .vcxproj.user files.",
 			action = "store_true",
 		)
@@ -163,7 +168,7 @@ class project_generator_visual_studio( project_generator.project_generator ):
 		self._collectProjects()
 
 
-	def _collectProjects(self):
+	def _collectProjects( self ):
 
 		def recurseGroups( projectMap_out, parentFilter_out, projectOutputPath, projectGroup ):
 			# Setup the projects first.
@@ -185,89 +190,89 @@ class project_generator_visual_studio( project_generator.project_generator ):
 				for toolchainName, configMap in projectSettingsMap.items():
 					for configName, archMap in configMap.items():
 						for archName, settings in archMap.items():
-							configName = CorrectConfigName(configName)
+							configName = CorrectConfigName( configName )
 							projectData.platformConfigList.append( ( configName, Platform( toolchainName, archName ), toolchainName, settings ) )
-							projectData.fullSourceFileList.update(set(settings.allsources))
-							projectData.fullHeaderFileList.update(set(settings.allheaders))
-							projectData.fullIncludePathList.update(set(settings.includeDirs))
+							projectData.fullSourceFileList.update( set( settings.allsources ) )
+							projectData.fullHeaderFileList.update( set( settings.allheaders ) )
+							projectData.fullIncludePathList.update( set( settings.includeDirs ) )
 							for source in projectData.fullSourceFileList:
-								projectData.fileFilterMap[source] = os.path.join("Source Files", os.path.dirname(os.path.relpath(source, settings.workingDirectory)).replace(".." + os.path.sep, "")).rstrip(os.path.sep)
+								projectData.fileFilterMap[source] = os.path.join( "Source Files", os.path.dirname( os.path.relpath( source, settings.workingDirectory ) ).replace( ".." + os.path.sep, "" ) ).rstrip( os.path.sep )
 							for header in projectData.fullHeaderFileList:
-								projectData.fileFilterMap[header] = os.path.join("Header Files", os.path.dirname(os.path.relpath(header, settings.workingDirectory)).replace(".." + os.path.sep, "")).rstrip(os.path.sep)
+								projectData.fileFilterMap[header] = os.path.join( "Header Files", os.path.dirname( os.path.relpath( header, settings.workingDirectory ) ).replace( ".." + os.path.sep, "" ) ).rstrip( os.path.sep )
 
-				projectData.fullSourceFileList = sorted(projectData.fullSourceFileList)
-				projectData.fullHeaderFileList = sorted(projectData.fullHeaderFileList)
-				projectData.platformConfigList = sorted(projectData.platformConfigList)
+				projectData.fullSourceFileList = sorted( projectData.fullSourceFileList )
+				projectData.fullHeaderFileList = sorted( projectData.fullHeaderFileList )
+				projectData.platformConfigList = sorted( projectData.platformConfigList )
 
 				# Grab the projectSettings for the first key in the config map and fill in a set of names for dependent projects.
 				# We only need the names for now. We can handle resolving them after we have all of the projects mapped.
 
-				configMap = list(projectSettingsMap.values())[0]
-				archMap = list(configMap.values())[0]
-				settings = list(archMap.values())[0]
+				configMap = list( projectSettingsMap.values() )[0]
+				archMap = list( configMap.values() )[0]
+				settings = list( archMap.values() )[0]
 				for dependentProjectKey in settings.linkDepends:
-					projectData.dependencyList.add(_shared_globals.projects[dependentProjectKey].name)
+					projectData.dependencyList.add( _shared_globals.projects[dependentProjectKey].name )
 
 				# Grab the path to this project's makefile.
 				projectData.makefilePath = settings.scriptFile
 
-				projectData.dependencyList = set(sorted(projectData.dependencyList)) # Sort the dependency list.
+				projectData.dependencyList = set( sorted( projectData.dependencyList ) ) # Sort the dependency list.
 				projectMap_out[projectData.name] = projectData
 
 			# Sort the parent filter dependency list.
 			if parentFilter_out:
-				parentFilter_out.dependencyList = set(sorted(parentFilter_out.dependencyList))
+				parentFilter_out.dependencyList = set( sorted( parentFilter_out.dependencyList ) )
 
 			# Next, iterate through each subgroup and handle each one recursively.
 			for subGroupName, subGroup in projectGroup.subgroups.items():
-				groupPath = os.path.join(projectOutputPath, subGroupName)
+				groupPath = os.path.join( projectOutputPath, subGroupName )
 
-				groupPathFinal = os.path.join(self.rootpath, groupPath)
+				groupPathFinal = os.path.join( self.rootpath, groupPath )
 
-				filterData = Project(subGroupName, self._projectUuidList) # Subgroups should be treated as project filters in the solution.
+				filterData = Project( subGroupName, self._projectUuidList ) # Subgroups should be treated as project filters in the solution.
 				filterData.isFilter = True
 
 				# Explicitly map the filter names with a different name to help avoid possible naming collisions.
-				projectMap_out["{}.Filter".format(filterData.name)] = filterData
+				projectMap_out["{}.Filter".format( filterData.name )] = filterData
 
 				# Create the group path if it doesn't exist.
-				if not os.access(groupPathFinal, os.F_OK):
-					os.makedirs(groupPathFinal)
+				if not os.access( groupPathFinal, os.F_OK ):
+					os.makedirs( groupPathFinal )
 
-				recurseGroups(projectMap_out, filterData, groupPath, subGroup)
+				recurseGroups( projectMap_out, filterData, groupPath, subGroup )
 
 
-		def resolveDependencies(projectMap):
+		def resolveDependencies( projectMap ):
 			for projectId, projectData in projectMap.items():
 				resolvedDependencyList = []
 
 				# Sort the dependency name list before parsing it.
-				projectData.dependencyList = sorted(projectData.dependencyList)
+				projectData.dependencyList = sorted( projectData.dependencyList)
 
 				# Resolve each project name to their associated project objects.
 				for dependentProjectName in projectData.dependencyList:
-					resolvedDependencyList.append(projectMap[dependentProjectName])
+					resolvedDependencyList.append( projectMap[dependentProjectName] )
 
 				# Replace the old name list with the new resolved list.
 				projectData.dependencyList = resolvedDependencyList
 
 
 		# Create the base output directory if necessary.
-		if not os.access(self.rootpath, os.F_OK):
-			os.makedirs(self.rootpath)
+		if not os.access( self.rootpath, os.F_OK ):
+			os.makedirs( self.rootpath )
 
 		# When not creating a native project, a custom project must be injected in order to achieve full solution builds
 		# since Visual Studio doesn't give us a way to override the behavior of the "Build Solution" command.
 		if not self._createNativeProject:
 			# Fill in the project data.
 			buildAllProjectName = "(BUILD_ALL)"
-			buildAllProjectData = Project(buildAllProjectName, self._projectUuidList) # Create a new object to contain project data.
+			buildAllProjectData = Project( buildAllProjectName, self._projectUuidList ) # Create a new object to contain project data.
 			buildAllProjectData.outputPath = self.rootpath
 			buildAllProjectData.isBuildAllProject = True
 			buildAllProjectData.makefilePath = csbuild.scriptFiles[0] # Reference the main makefile.
 
 			regenProjectName = "(REGENERATE_SOLUTION)"
-			regenProjectData = Project(regenProjectName, self._projectUuidList) # Create a new object to contain project data.
+			regenProjectData = Project( regenProjectName, self._projectUuidList ) # Create a new object to contain project data.
 			regenProjectData.outputPath = self.rootpath
 			regenProjectData.isRegenProject = True
 			regenProjectData.makefilePath = csbuild.scriptFiles[0] # Reference the main makefile.
@@ -275,30 +280,30 @@ class project_generator_visual_studio( project_generator.project_generator ):
 			# The Build All project doesn't have any project settings, but it still needs all of the platforms and configurations.
 			for platformName in self._platformList:
 				for configName in self._configList:
-					buildAllProjectData.platformConfigList.append((configName, platformName, "", None))
-					regenProjectData.platformConfigList.append((configName, platformName, "", None))
+					buildAllProjectData.platformConfigList.append( ( configName, platformName, "", None ) )
+					regenProjectData.platformConfigList.append( ( configName, platformName, "", None ) )
 
 			# Add the Build All and Regen projects to the project map.
 			self._projectMap[buildAllProjectData.name] = buildAllProjectData
 			self._projectMap[regenProjectData.name] = regenProjectData
 
-		recurseGroups(self._projectMap, None, "", projectSettings.rootGroup)
-		resolveDependencies(self._projectMap)
+		recurseGroups( self._projectMap, None, "", projectSettings.rootGroup )
+		resolveDependencies( self._projectMap )
 
 		# Copy the project names into a list.
 		for projectName, projectData in self._projectMap.items():
-			self._orderedProjectList.append(projectName)
+			self._orderedProjectList.append( projectName )
 
 		# Sort the list of project names.
-		self._orderedProjectList = sorted(self._orderedProjectList)
+		self._orderedProjectList = sorted( self._orderedProjectList )
 
 		# Replace the list of names in the project list with the actual project data.
-		for i in range(0, len(self._orderedProjectList)):
+		for i in range( 0, len( self._orderedProjectList ) ):
 			projectName = self._orderedProjectList[i]
 			self._orderedProjectList[i] = self._projectMap[projectName]
 
 		# Create a single list of every include search path.
 		for projectName, projectData in self._projectMap.items():
-			self._fullIncludePathList.update(projectData.fullIncludePathList)
+			self._fullIncludePathList.update( projectData.fullIncludePathList )
 
-		self._fullIncludePathList = sorted(self._fullIncludePathList)
+		self._fullIncludePathList = sorted( self._fullIncludePathList )
---
+++ csbuild/vsgen/platform_android.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2013 Jaedyn K. Draper
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Contains the vsgen platform for Tegra-Android."""
+
+from .platform_base import PlatformBase
+
+
+class PlatformTegraAndroid( PlatformBase ):
+	def __init__( self ):
+		PlatformBase.__init__( self )
+
+
+	def GetEntryName( self ):
+		"""
+		Retrieve the name value that will show up in Visual Studio as a buildable platform for a generated project.  Must be a name that Visual Studio recognizes.
+
+		:return: str
+		"""
+		return "Tegra-Android"
