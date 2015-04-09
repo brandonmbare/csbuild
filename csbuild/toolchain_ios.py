41bdae65a2f0c3b762a882efbf9795250ca0fd93
Brandon Bare <brandon.m.bare@gmail.com>
2015-04-09 13:20:56 -0500

    Fixed the iOS toolchain having hardcoded paths to the SDK directories. They now use xcrun to determine the default directories, though they can still be overridden by setting the target device/simulator version in the makefile.

--- csbuild/toolchain_ios.py
+++ csbuild/toolchain_ios.py
@@ -23,9 +23,18 @@ Contains a plugin class for building iOS projects.
 """
 
 import csbuild
+import subprocess
+import sys
 
 from . import toolchain_gcc
 from . import toolchain_gcc_darwin
+
+
+HAS_RUN_XCRUN = False
+DEFAULT_DEVICE_SDK_DIR = None
+DEFAULT_SIMULATOR_SDK_DIR = None
+DEFAULT_DEVICE_SDK_VERSION = None
+DEFAULT_SIMULATOR_SDK_VERSION = None
 
 
 class iOSArchitecture( object ):
@@ -37,11 +46,49 @@ class iOSArchitecture( object ):
 
 class iOSBase( object ):
 	def __init__( self ):
-		self.shared._targetDeviceVersion = "8.1"
-		self.shared._targetSimulatorVersion = "8.1"
+		global HAS_RUN_XCRUN
+		if not HAS_RUN_XCRUN:
+			global DEFAULT_DEVICE_SDK_DIR
+			global DEFAULT_SIMULATOR_SDK_DIR
+			global DEFAULT_DEVICE_SDK_VERSION
+			global DEFAULT_SIMULATOR_SDK_VERSION
+
+			try:
+				DEFAULT_DEVICE_SDK_DIR = subprocess.check_output(["xcrun", "--sdk", "iphoneos", "--show-sdk-path"])
+				DEFAULT_SIMULATOR_SDK_DIR = subprocess.check_output(["xcrun", "--sdk", "iphonesimulator", "--show-sdk-path"])
+			except:
+				DEFAULT_DEVICE_SDK_DIR = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
+				DEFAULT_SIMULATOR_SDK_DIR = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk"
+
+			try:
+				DEFAULT_DEVICE_SDK_VERSION = subprocess.check_output(["xcrun", "--sdk", "iphoneos", "--show-sdk-version"])
+				DEFAULT_SIMULATOR_SDK_VERSION = subprocess.check_output(["xcrun", "--sdk", "iphonesimulator", "--show-sdk-version"])
+			except:
+				DEFAULT_DEVICE_SDK_VERSION = ""
+				DEFAULT_SIMULATOR_SDK_VERSION = ""
+
+			if sys.version_info >= (3, 0):
+				DEFAULT_DEVICE_SDK_DIR = DEFAULT_DEVICE_SDK_DIR.decode("utf-8")
+				DEFAULT_SIMULATOR_SDK_DIR = DEFAULT_SIMULATOR_SDK_DIR.decode("utf-8")
+				DEFAULT_DEVICE_SDK_VERSION = DEFAULT_DEVICE_SDK_VERSION.decode("utf-8")
+				DEFAULT_SIMULATOR_SDK_VERSION = DEFAULT_SIMULATOR_SDK_VERSION.decode("utf-8")
+
+			DEFAULT_DEVICE_SDK_DIR = DEFAULT_DEVICE_SDK_DIR.strip("\n")
+			DEFAULT_SIMULATOR_SDK_DIR = DEFAULT_SIMULATOR_SDK_DIR.strip("\n")
+			DEFAULT_DEVICE_SDK_VERSION = DEFAULT_DEVICE_SDK_VERSION.strip("\n")
+			DEFAULT_SIMULATOR_SDK_VERSION = DEFAULT_SIMULATOR_SDK_VERSION.strip("\n")
+
+			HAS_RUN_XCRUN = True
+
+		self.shared._deviceSdkDir = DEFAULT_DEVICE_SDK_DIR
+		self.shared._simulatorSdkDir = DEFAULT_SIMULATOR_SDK_DIR
+		self.shared._targetDeviceVersion = DEFAULT_DEVICE_SDK_VERSION
+		self.shared._targetSimulatorVersion = DEFAULT_SIMULATOR_SDK_VERSION
 
 
 	def _copyTo( self, other ):
+		other.shared._deviceSdkDir = self.shared._deviceSdkDir
+		other.shared._simulatorSdkDir = self.shared._simulatorSdkDir
 		other.shared._targetDeviceVersion = self.shared._targetDeviceVersion
 		other.shared._targetSimulatorVersin = self.shared._targetSimulatorVersion
 
@@ -61,6 +108,7 @@ class iOSBase( object ):
 		:param version: Target device version.
 		:type version: str
 		"""
+		self.shared._deviceSdkDir = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS{}.sdk".format( version )
 		self.shared._targetDeviceVersion = version
 
 
@@ -71,6 +119,7 @@ class iOSBase( object ):
 		:param version: Target simulator version.
 		:type version: str
 		"""
+		self.shared._simulatorSdkDir = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator{}.sdk".format( version )
 		self.shared._targetSimulatorVersion = version
 
 
@@ -84,10 +133,10 @@ class iOSBase( object ):
 
 	def _getMinVersionArg( self, arch ):
 		argumentMap = {
-			iOSArchitecture.DEVICE_ARMV7:   "-miphoneos-version-min={} ".format( self.shared._targetDeviceVersion ),
-			iOSArchitecture.DEVICE_ARM64:   "-miphoneos-version-min={} ".format( self.shared._targetDeviceVersion ),
-			iOSArchitecture.SIMULATOR_I386: "-mios-simulator-version-min={} ".format( self.shared._targetSimulatorVersion ),
-			iOSArchitecture.SIMULATOR_X64:  "-mios-simulator-version-min={} ".format( self.shared._targetSimulatorVersion ),
+			iOSArchitecture.DEVICE_ARMV7:   "-miphoneos-version-min={} ".format( self.shared._targetDeviceVersion ) if self.shared._targetDeviceVersion else "",
+			iOSArchitecture.DEVICE_ARM64:   "-miphoneos-version-min={} ".format( self.shared._targetDeviceVersion ) if self.shared._targetDeviceVersion else "",
+			iOSArchitecture.SIMULATOR_I386: "-mios-simulator-version-min={} ".format( self.shared._targetSimulatorVersion ) if self.shared._targetSimulatorVersin else "",
+			iOSArchitecture.SIMULATOR_X64:  "-mios-simulator-version-min={} ".format( self.shared._targetSimulatorVersion ) if self.shared._targetSimulatorVersin else "",
 		}
 		return argumentMap[arch]
 
@@ -112,10 +161,10 @@ class iOSBase( object ):
 
 	def _setSysRoot( self, arch ):
 		sysRootMap = {
-			iOSArchitecture.DEVICE_ARMV7:   "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS{}.sdk".format( self.shared._targetDeviceVersion ),
-			iOSArchitecture.DEVICE_ARM64:   "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS{}.sdk".format( self.shared._targetDeviceVersion ),
-			iOSArchitecture.SIMULATOR_I386: "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator{}.sdk".format( self.shared._targetSimulatorVersion ),
-			iOSArchitecture.SIMULATOR_X64:  "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator{}.sdk".format( self.shared._targetSimulatorVersion ),
+			iOSArchitecture.DEVICE_ARMV7:   self.shared._deviceSdkDir,
+			iOSArchitecture.DEVICE_ARM64:   self.shared._deviceSdkDir,
+			iOSArchitecture.SIMULATOR_I386: self.shared._simulatorSdkDir,
+			iOSArchitecture.SIMULATOR_X64:  self.shared._simulatorSdkDir,
 		}
 		self.shared._sysroot = sysRootMap[arch]
 
