bf5430c6b128559cc3e91a272ca87810e565df74
brandonmbare <brandon.m.bare@gmail.com>
2015-08-21 16:59:11 -0500

    Removing the quotes around the library directories for the gcc toolchain until we find a way to handle that in a way ld can handle.

--- csbuild/toolchain_gcc.py
+++ csbuild/toolchain_gcc.py
@@ -517,13 +517,13 @@ class GccLinker( gccBase, toolchain.linkerBase ):
 		"""Returns a string containing all of the passed library dirs, formatted to be passed to gcc/g++."""
 		ret = ""
 		for lib in libDirs:
-			ret += '-L"{}" '.format( lib )
+			ret += '-L{} '.format( lib )
 		ret += "-L/usr/lib -L/usr/local/lib "
 		if self._include_lib64:
 			ret += "-L/usr/lib64 -L/usr/local/lib64 "
 		if forLinker:
 			for lib in libDirs:
-				ret += '-Wl,-R"{}" '.format( os.path.abspath( lib ) )
+				ret += '-Wl,-R{} '.format( os.path.abspath( lib ) )
 			ret += "-Wl,-R/usr/lib -Wl,-R/usr/local/lib "
 			if self._include_lib64:
 				ret += "-Wl,-R/usr/lib64 -Wl,-R/usr/local/lib64 "
